95104401a3d3a202adeca621ce0a3a51fe8d3325
Yulia Tenincheva <yulia.ten@dezigner.ru>
2015-06-16 15:51:43 +0300

    Fixes - tests & solution

--- JavaScript-OOP/01.Functions/tasks/task-1.js
+++ JavaScript-OOP/01.Functions/tasks/task-1.js
@@ -1,49 +1,49 @@
 /* Task Description */
 /* 
-	Write a function that sums an array of numbers:
-		numbers must be always of type Number
-		returns `null` if the array is empty
-		throws Error if the parameter is not passed (undefined)
-		throws if any of the elements is not convertible to Number	
+ Write a function that sums an array of numbers:
+ numbers must be always of type Number
+ returns `null` if the array is empty
+ throws Error if the parameter is not passed (undefined)
+ throws if any of the elements is not convertible to Number
 
-*/
+ */
 
 function sum(items) {
-	'use strict';
+    'use strict';
 
-	return items
-		.validate(isNumber)
-		.map(Number)
-		.reduce(function (a, b) {
-			return a + b;
-		}) || null;
+    return items
+            .validateNumbers()
+            .map(Number)
+            .reduce(function (a, b) {
+                return a + b;
+            }) || null;
 }
 
 /**
  * --- ES6 basic solution ---
  *     You can open it in Firefox or with iojs (using --harmony_arrow_functions flag) **/
- //var sum = (n) => n.validate(isNumber).map(Number).reduce((a, b) => a + b) || null; // <3
+//var sum = (n) => n.validate(isNumber).map(Number).reduce((a, b) => a + b) || null; // <3
 
 /** --- Helper functions --- **/
 
-Array.prototype.validate = function(validateWith){
-	if (!this) {
-		throw new Error('Parameter is not passed');
-	}
+Array.prototype.validateNumbers = function () {
+    if (typeof this === 'undefined') {
+        throw new Error('Parameter is not passed');
+    }
 
-	if (this.length < 1) {
-		return [null];
-	}
+    if (this.length < 1) {
+        return [null];
+    }
 
-	if (!this.every(validateWith)) {
-		throw new Error('Array contains a non-convertible to Number element');
-	}
+    if (!this.every(isNumber)) {
+        throw new Error('Array contains a non-convertible to Number element');
+    }
 
-	return this;
+    return this;
 };
 
 function isNumber(item) {
-	return !(isNaN(item) || Number(item).toString() !== item.toString());
+    return !(isNaN(item) || Number(item).toString() !== item.toString());
 }
 
 module.exports = sum;
--- JavaScript-OOP/01.Functions/tasks/task-2.js
+++ JavaScript-OOP/01.Functions/tasks/task-2.js
@@ -1,61 +1,60 @@
 /* Task description */
 /*
-	Write a function a function that finds all the prime numbers in a range
-		1) it should return the prime numbers in an array
-		2) it must throw an Error if any on the range params is not convertible to `string`
-		3) it must throw an Error if any of the range params is missing
-*/
+ Write a function a function that finds all the prime numbers in a range
+ 1) it should return the prime numbers in an array
+ 2) it must throw an Error if any on the range params is not convertible to `string`
+ 3) it must throw an Error if any of the range params is missing
+ */
 
-function solve(from, to) {
-	'use strict';
+function findPrimesInRange(from, to) {
+    'use strict';
 
-	return [from, to].validate(isNumber).range().filter(isPrime);
-}
-
-function isPrime(number) {
-	var start = 2;
-
-	/** Bitwise Hacks:
-	 *  So, Math.pow(2, 2) is === 2 << 1
-	 *  Then, sqrt should be  === 2 >> 1
-	 * */
-
-	while (start <= (number >> 2)) {
-		if (number % start++ < 1) {
-			return false;
-		}
-	}
-
-	return number > 1 && number !== 4;
+    return ([from, to].validate().range().filter(isPrime));
 }
 
 // Generate array of consecutive numbers
-// Actually, my fav is that one: http://stackoverflow.com/a/25166457/4483717
-Array.prototype.range = function() {
-	var start = Number(this[0]);
-	var count = Number(this[1]) - start + 1;
+Array.prototype.range = function () {
+    var start = Number(this[0]);
+    var count = Number(this[1]) - start + 1;
 
-	return Array
-		.apply(0, Array(count))
-		.map(function (element, index) {
-			return index + start;
-		});
+    return Array
+        .apply(0, Array(count))
+        .map(function (element, index) {
+            return index + start;
+        });
 };
 
-Array.prototype.validate = function(validateWith){
-	if (!this) {
-		throw new Error('Some range params are missing (from, to)');
-	}
+Array.prototype.validate = function () {
+    if (this.length < 2 || typeof this === 'undefined') {
+        throw new Error('Some range params are missing (from, to)');
+    }
 
-	if (!this.every(validateWith)) {
-		throw new Error('Array contains a non-convertible to Number element');
-	}
+    if (!this.every(isNumber)) {
+        throw new Error('Array contains a non-convertible to Number element');
+    }
 
-	return this;
+    return this;
 };
 
-function isNumber(item){
-	return !(isNaN(item) || Number(item).toString() !== item.toString());
+function isPrime(number) {
+    var start = 2;
+
+    /** Bitwise Hacks:
+     *  So, Math.pow(2, 2) is === 2 << 1
+     *  Then, sqrt should be  === 2 >> 1
+     * */
+
+    while (start <= (number >> 2)) {
+        if (number % start++ < 1) {
+            return false;
+        }
+    }
+
+    return (number > 1 && number !== 4 && number);
 }
 
-module.exports = solve;
+function isNumber(item) {
+    return !(isNaN(item) || Number(item).toString() !== item.toString());
+}
+
+module.exports = findPrimesInRange;
